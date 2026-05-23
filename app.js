fba39341dd161bde9e53661267960900893782fb
galcohen7 <galc1702@gmail.com>
2026-05-23 14:31:41 +0300

    Merge local files with GitHub init files

---
+++ app.js
@@ -0,0 +1,12 @@
+import { createServer } from "http";
+
+createServer((request, response) => {
+    if (request.url === '/foo') {
+        response.write('<html><body><b>foo</b></body></html>');
+    } else if (request.url === '/bar') {
+        response.write('<html><body><u>bar</u></body></html>');
+    } else {
+        response.statusCode = 404;
+        response.end();
+    }
+}).listen(80);
